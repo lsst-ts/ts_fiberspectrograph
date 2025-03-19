090272e698446b89a4367eea7e3a8c104956f32e
Eric Coughlin <ecoughlin@lsst.org>
2025-03-19 10:30:26 -0300

    Use URL returned by upload method.

--- python/lsst/ts/fiberspectrograph/csc.py
+++ python/lsst/ts/fiberspectrograph/csc.py
@@ -279,8 +279,7 @@ class FiberSpectrographCsc(salobj.ConfigurableCsc):
             suffix=".fits",
         )
         try:
-            await self.s3bucket.upload(fileobj=fileobj, key=key)
-            url = f"{self.s3bucket.service_resource.meta.client.meta.endpoint_url}/{self.s3bucket.name}/{key}"
+            url = await self.s3bucket.upload(fileobj=fileobj, key=key)
             await self.evt_largeFileObjectAvailable.set_write(
                 url=url, generator=self.generator_name
             )
